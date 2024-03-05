a4bb92ae46012918694fc5cedcf070651deaf92a
guilherme <guilhermesilvas2004@gmail.com>
2024-03-05 00:13:22 -0300

    manipulando classe

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
-
 const titulo = document.querySelector('.app__title')
+
+const botoes = document.querySelectorAll('.app__card-button')
 
 // Após realizar a refatoração e substituir essa parte por um função 
 // focoBt.addEventListener('click', () => {
@@ -23,15 +24,28 @@ const titulo = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
+
+
+
 })
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
+    
+    
 })
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
+    
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach( function (contexto){
+        contexto.classList.remove('active')
+    })
+
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
