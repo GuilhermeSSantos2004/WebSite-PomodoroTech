b50d9d2ec9cdcbd9cf18e50f5c1e28e699dd2144
guilherme <guilhermesilvas2004@gmail.com>
2024-03-05 00:53:45 -0300

    fix bug func contagemregressiva

--- script.js
+++ script.js
@@ -1,66 +1,47 @@
 const html = document.querySelector('html')
-
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
-
 const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+
+
+let tempoDecorridoEmSegundos = 5
 let intervaloId = null
 
-const musicaFocoInput = document.querySelector('#alternar-musica');
-const musica = new Audio('sons/luna-rise-part-one.mp3');
+musica.loop = true
 
-musicaFocoInput.addEventListener('change', () {
+musicaFocoInput.addEventListener('change', () => {
     if(musica.paused) {
-            musica.play()
-        } else {
-            musica.pause()
-        }
+        musica.play()
+    } else {
+        musica.pause()
+    }
 })
-
-// Após realizar a refatoração e substituir essa parte por um função 
-// focoBt.addEventListener('click', () => {
-//     html.setAttribute('data-contexto', 'foco')
-//     banner.setAttribute('src', '/imagens/foco.png')
-// })
-// curtoBt.addEventListener('click', () => {
-//     html.setAttribute('data-contexto', 'descanso-curto')
-//     banner.setAttribute('src', '/imagens/descanso-curto.png')
-// })
-// longoBt.addEventListener('click', () => {
-//     html.setAttribute('data-contexto', 'descanso-longo')
-//     banner.setAttribute('src', '/imagens/descanso-longo.png')
-// })
-
-
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
     focoBt.classList.add('active')
-
-
-
 })
+
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
-    
-    
 })
+
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
-    
 })
 
 function alterarContexto(contexto) {
-    botoes.forEach( function (contexto){
+    botoes.forEach(function (contexto){
         contexto.classList.remove('active')
     })
-
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -85,15 +66,29 @@ function alterarContexto(contexto) {
 }
 
 const contagemRegressiva = () => {
-    iniciar()
+    if(tempoDecorridoEmSegundos <= 0){
+        audioTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        zerar()
+        return
+    }
     tempoDecorridoEmSegundos -= 1
     console.log('Temporizador: ' + tempoDecorridoEmSegundos)
 }
 
-function iniciar() {
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        
+        zerar()
+        return
+    }
+    
     intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
-
-
-
+function zerar() {
+    clearInterval(intervaloId) 
+    intervaloId = null
+}
